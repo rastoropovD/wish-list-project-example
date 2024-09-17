b2c8af7c718c3eb9c6262a04d43aeb3bb8cfaff8
Dmytro Rastoropov <32164967+rastoropovD@users.noreply.github.com>
2024-09-17 21:44:37 +0300

    Update RepositoryFactory.cs

--- WishList.PostgreSQL/Data/Repositories/Factory/RepositoryFactory.cs
+++ WishList.PostgreSQL/Data/Repositories/Factory/RepositoryFactory.cs
@@ -8,6 +8,36 @@ namespace WishList.PostgreSQL.Data.Repositories.Factory;
 
 public sealed class RepositoryFactory : IRepositoryFactory
 {
+    // private instance - single instance
+    private IRepositoryFactory _instance;
+
+    // locker object for thread safe instance creation
+    private static readonly object Locker = new object();
+    
+
+    // private constructor to restrict object creation from outside
+    private RepositoryFactory() {}
+
+    // static method for getting single instance of the factory
+    public static IRepositoryFactory Instance() 
+    {
+        if(_instance != null) 
+        {
+            return _instance;
+        }
+
+        lock(Locker)
+        {
+            if(_instance == null) 
+            {
+                _instance = new RepositoryFactory();
+            }
+        }
+
+        return _instance;
+        
+    }
+    
     public IRepository Instantiate<TEntity>(WishListDbContext context) where TEntity : class
     {
         return typeof(TEntity).Name switch
